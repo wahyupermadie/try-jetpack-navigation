196348f209f13531792131331cf2cae96a8a6bde
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2019-09-02 13:11:27 +0700

    [ADD] navigation

--- app/src/main/java/com/example/latihanjetpack/MainActivity.kt
+++ app/src/main/java/com/example/latihanjetpack/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.latihanjetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val host = NavHostFragment.create(R.navigation.navigation)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, host)
+            .setPrimaryNavigationFragment(host).commit()
     }
 }
